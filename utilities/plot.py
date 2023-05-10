ba79f6182713987382709e8edfd5d915472d316e
justinjmueller <justin.mueller@colostate.edu>
2023-05-10 10:45:26 -0500

    Added function for plotting the physical wires colored by a metric

--- utilities/plot.py
+++ utilities/plot.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from matplotlib import colors as colors
@@ -92,6 +93,63 @@ def plot_crate(datasets, labels, metric='rawrms', component='WW19') -> None:
     ax.set_ylabel('RMS [ADC]')
     ax.legend()
     figure.suptitle(title)
+
+def plot_wire_planes(data, metric, metric_label, tpc=0):
+    """
+    Create and save a plot displaying a per-plane view with each wire
+    in its physical location colored by the specified metric.
+
+    Parameters
+    ----------
+    data: Dataset
+        The dataset containing the data.
+    metric: str
+        The name of the metric to be plotted.
+    metric_label: str
+        The label for the metric.
+    tpc: int
+        The desired TPC to display (0, 1, 2, 3 = EE, EW, WE, WW).
+
+    Returns
+    -------
+    None.
+    """
+    plt.style.use('plot_style.mplstyle')
+    figure = plt.figure(figsize=(20,12))
+    gspec = figure.add_gridspec(ncols=10, nrows=3)
+    axs = [figure.add_subplot(gspec[i, :9]) for i in [0,1,2]]
+    cmap = plt.cm.ScalarMappable(cmap=cm.viridis, norm=colors.Normalize(0,1))
+    cmap.set_clim(0,10)
+    plane_label = {0: 'Induction 1', 1: 'Induction 2', 2: 'Collection'}
+
+    for p in [0,1,2]:
+        mask = data.chmap['channel_id'] // 13824 == tpc
+        wires = data.chmap.loc[mask][['channel_id', 'z0', 'y0', 'z1', 'y1']]
+        tmp = pd.DataFrame({'channel_id': data['channel_id'], metric: data[metric]}).loc[data['plane'] == p]
+        wires = wires.merge(tmp, how='inner', left_on='channel_id', right_on='channel_id')
+        cs = cmap.to_rgba(wires[metric])
+        wires = [[(x[1], x[2]), (x[3], x[4])] for x in wires.to_numpy()]
+        axs[p].add_collection(LineCollection(wires, colors=cs))
+        axs[p].set_xlim(-895.95, 895.95)
+        axs[p].set_ylim(-181.70, 134.80)
+        axs[p].set_xlabel('Z Position [cm]')
+        axs[p].set_ylabel('Y Position [cm]')
+        axs[p].set_title(plane_label[p])
+    cax = figure.add_axes([0.92, 0.05, 0.035, 0.89])
+    axcb = figure.colorbar(cmap, ax=axs, cax=cax, use_gridspec=True)
+    axcb.set_label(metric_label)
+    axcb.solids.set_edgecolor('face')
+    tpc_name = {0: 'EE', 1: 'EW', 2: 'WE', 3: 'WW'}
+    figure.suptitle(f'{metric_label.split(" [")[0]} by Plane for {tpc_name[tpc]}')
+
+"""
+    axcb.set_label(f'Status', fontsize=18)
+    axcb.solids.set_edgecolor('face')
+    cax.tick_params(labelsize=16)
+    tpc_name = {0: 'EE', 1: 'EW', 2: 'WE', 3: 'WW'}[tpc]
+    figure.suptitle(f'Channel Status of TPC: {tpc_name}',
+                    x=0.465, fontsize=24)
+"""
 
 def plot_waveform(waveform, title) -> None:
     """
