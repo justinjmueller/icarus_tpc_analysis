377cb8d4036077d69f9130f67abb2e861c91214c
justinjmueller <justin.mueller@colostate.edu>
2023-11-07 14:06:19 -0600

    Made some changes to the INFNDataset class to enable comparisons

--- noise/infn/infn.py
+++ noise/infn/infn.py
@@ -4,6 +4,8 @@ import struct
 from glob import glob
 import matplotlib.pyplot as plt
 import sys
+from tqdm import tqdm
+from scipy.signal import periodogram
 
 sys.path.append('..')
 from globals import *
@@ -23,12 +25,52 @@ class INFNDataset:
         None.
         """
         plt.style.use(PLOT_STYLE)
-        inputs = glob(f'{path}/*')
-        if len(inputs) > nevts:
-            inputs = inputs[:nevts]
-        self.events = [INFNEvent(x) for x in inputs]
-        self.covariance = np.mean([x.cov for x in self.events], axis=0)
-        self._get_noise()
+        if '.npz' in path:
+            self._read(path)
+        else:
+            inputs = glob(f'{path}/*')
+            if len(inputs) > nevts:
+                inputs = inputs[:nevts]
+            self.events = [INFNEvent(x) for x in tqdm(inputs)]
+            self.covariance = np.mean([x.cov for x in self.events], axis=0)
+            self._get_noise()
+            self._get_ffts()
+
+    def write(self, path) -> None:
+        """
+        Saves the dataset to a npz file.
+
+        Parameters
+        ----------
+        path: str
+            The full path to save the dataset to.
+
+        Results
+        -------
+        None.
+        """
+        noise_data = self.median_noise_data[['slot_id', 'local_id', 'pedestal', 'raw_rms']].to_numpy()
+        np.savez(path, median_noise_data=noise_data, covariance=self.covariance)
+
+    def _read(self, path) -> None:
+        """
+        Initializes the dataset by reading in the results from an npz
+        file.
+
+        Parameters
+        ----------
+        path: str
+            The full path of the input npz file containing the previous
+            analysis results
+
+        Returns
+        -------
+        None.
+        """
+        input_file = np.load(path)
+        self.median_noise_data = pd.DataFrame(input_file['median_noise_data'], columns=['slot_id', 'local_id', 'pedestal', 'raw_rms'])
+        self.median_noise_data['flange'] = 'infn'
+        self.covariance = input_file['covariance']
     
     def _get_noise(self) -> None:
         """
@@ -49,6 +91,21 @@ class INFNDataset:
                 'pedestal': np.median(np.vstack([x.pedestals for x in self.events]), axis=0),
                 'raw_rms': np.median(np.vstack([x.rms for x in self.events]), axis=0)}
         self.median_noise_data = pd.DataFrame(data)
+
+    def _get_ffts(self) -> None:
+        """
+        Calculates the average FFTs per board from the list of INFN
+        events and stores it as a class attribute.
+
+        Parameters
+        ----------
+        None.
+
+        Returns
+        -------
+        None.
+        """
+        self.ffts = np.mean(np.stack([x.ffts for x in self.events], axis=0), axis=0)
     
     def __getitem__(self, key) -> np.array:
         """
@@ -65,14 +122,17 @@ class INFNDataset:
         """
         return self.median_noise_data[key].to_numpy()
 
-    def plot_correlation_matrix(self) -> None:
+    def plot_correlation_matrix(self, override_title='', boards=9) -> None:
         """
         Plot the channel-to-channel correlation matrix of the test-
         stand data.
 
         Parameters
         ----------
-        None.
+        override_title: str
+            Overrides the default title of the plot if set.
+        boards: int
+            The number of boards to include in the plot.
 
         Returns
         -------
@@ -82,9 +142,10 @@ class INFNDataset:
         ax = figure.add_subplot()
         im = ax.imshow(self.covariance, cmap='RdBu', vmin=-1.0, vmax=1.0)
         figure.colorbar(im, ax=ax)
-        ax.set_xticks([64*i for i in range(10)])
-        ax.set_yticks([64*i for i in range(10)])
-        figure.suptitle('INFN Test Stand Correlation Matrix')
+        ax.set_xticks([64*i for i in range(boards+1)])
+        ax.set_yticks([64*i for i in range(boards+1)])
+        figure.suptitle(override_title if override_title else 'INFN Test Stand Correlation Matrix')
+        plt.show()
 
 class INFNEvent:
     def __init__(self, path) -> None:
@@ -111,8 +172,11 @@ class INFNEvent:
         self.waveforms = np.vstack([self.read_data_tile(raw, x+28+8) for x in self.tokens['DATA']])
         self.pedestals = np.median(self.waveforms, axis=1)
         self.waveforms = self.waveforms - self.pedestals[:, None]
-        self.calc_rms()
-        self.calc_correlations()
+        self._calc_rms()
+        self._calc_correlations()
+        self.frequencies, self.ffts = periodogram(self.waveforms, fs=2.5e3, axis=1)
+        nboards = int(self.waveforms.shape[0] / 64)
+        self.ffts = np.array([np.mean(self.ffts[b*64:(b+1)*64, :], axis=0) for b in range(nboards)])
         
     @staticmethod
     def get_tile_locations(evt, token) -> list[int]:
@@ -165,7 +229,7 @@ class INFNEvent:
             waveforms[:,t] = struct.unpack(form,evt[start+t*128:start+(t+1)*128])
         return waveforms
     
-    def calc_rms(self) -> None:
+    def _calc_rms(self) -> None:
         """
         Calculate the RMS of each waveform. These are stored internal
         to the class.
@@ -180,7 +244,7 @@ class INFNEvent:
         """
         self.rms = np.sqrt(np.mean(np.square(self.waveforms), axis=1))
 
-    def calc_correlations(self) -> None:
+    def _calc_correlations(self) -> None:
         """
         Calculate the channel-to-channel correlation matrix for the
         event. This is stored internal to the class.
