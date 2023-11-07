0a5d3eec61015e24f828598e8727d0a2bd94b165
justinjmueller <justin.mueller@colostate.edu>
2023-11-07 14:08:38 -0600

    Refactored to load more information from database

--- noise/fnal/dataset.py
+++ noise/fnal/dataset.py
@@ -28,6 +28,7 @@ class Dataset:
         trimmed_keys = [x.split(';')[0] for x in data.keys()]
         if 'tpccorrelation' in trimmed_keys:
             self.correlations = data['tpccorrelation'].arrays(library='pd')
+            self.correlations_int = data['tpccorrelation_int'].arrays(library='pd')
         if 'tpcnoise' in trimmed_keys:
             self._get_noise(data['tpcnoise'].arrays(library='pd'))
         if 'raw_ffts' in trimmed_keys:
@@ -93,13 +94,13 @@ class Dataset:
 
         if metric in self.median_noise_data.columns:
             blank_mask = np.repeat(True, len(self.median_noise_data))
-            tpc_mask = (self.median_noise_data['tpc'] == tpc if tpc is not None else blank_mask)
-            plane_mask = (self.median_noise_data['plane'] == plane if plane is not None else blank_mask)
+            tpc_mask = (self.median_noise_data['tpc_number'] == tpc if tpc is not None else blank_mask)
+            plane_mask = (self.median_noise_data['plane_number'] == plane if plane is not None else blank_mask)
             channel_mask = (~np.isin(self.median_noise_data['channel_id'], reject_channels))
         else:
             blank_mask = np.repeat(True, len(self.noise_data))
-            tpc_mask = (self.noise_data['tpc'] == tpc if tpc is not None else blank_mask)
-            plane_mask = (self.noise_data['plane'] == plane if plane is not None else blank_mask)
+            tpc_mask = (self.noise_data['tpc_number'] == tpc if tpc is not None else blank_mask)
+            plane_mask = (self.noise_data['plane_number'] == plane if plane is not None else blank_mask)
             channel_mask = (~np.isin(self.noise_data['channel_id'], reject_channels))
         return tpc_mask & plane_mask & channel_mask
     
@@ -197,31 +198,28 @@ class Dataset:
         -------
         None.
         """
-        columns = list(input_df.columns)
+        columns = [x for x in list(input_df.columns) if x not in ['slot_id', 'group_id']]
         conn = sqlite3.connect(SQLITE_CHANNEL_MAP_PATH)
-        chmap = pd.read_sql_query("SELECT channel_id, flange_name, cable_number FROM channelinfo;", conn)
+        chmap = pd.read_sql_query("SELECT channel_id, tpc_number, plane_number, slot_id, flange_name, group_id, cable_number FROM channelinfo;", conn)
         wires = pd.read_sql_query('SELECT * FROM physicalwires', conn)
         cables = pd.read_sql_query('SELECT * FROM flatcables', conn)
         conn.close()
         
         cmap = lambda x: f'{x[0]}{((int(x[1:]) + 8) % 17):02}' if 'K' in x or 'F' in x else x
         cables['cable_number'] = [cmap(x) for x in cables['cable_number']]
-        original_columns = ['channel_id', 'flange_name', 'wire_capacitance', 'cable_capacitance']
+        original_columns = ['channel_id', 'tpc_number', 'plane_number', 'slot_id', 'flange_name', 'group_id', 'wire_capacitance', 'cable_capacitance']
         chmap = chmap.merge(wires, on='channel_id')
         chmap = chmap.merge(cables, on='cable_number')
         chmap.rename(columns={'capacitance_x': 'wire_capacitance', 'capacitance_y': 'cable_capacitance'}, inplace=True)
         
-        data = input_df.merge(chmap[original_columns], left_on='channel_id', right_on='channel_id')
+        data = input_df[columns].merge(chmap[original_columns], left_on='channel_id', right_on='channel_id')
         flange_map = dict(zip(data['fragment'], data['flange_name']))
-        data['plane'] = np.digitize(data['channel_id'] % 13824, [2304, 8064, 13824])
-        data['tpc'] = data['channel_id'].to_numpy() // 13824
         mask = (data['mhit_height'] <= 25)
         self.noise_data = data.loc[mask]
         total_capacitance = (self.noise_data['wire_capacitance'] + self.noise_data['cable_capacitance'] + 30) / 1000.0
         for k in ['raw_rms', 'int_rms', 'coh_rms']:
             self.noise_data[f'{k}_norm'] = self.noise_data[k] / total_capacitance
         self.median_noise_data = data.loc[mask].groupby('channel_id').median().reset_index()
-        self.median_noise_data['flange'] = [flange_map[x] for x in self.median_noise_data['fragment']]
         self.median_noise_data['hit_occupancy'] = data.groupby('channel_id').mean()['hits']
         group_e2e = self.noise_data.groupby('channel_id')
         group_c2c = self.noise_data.groupby(['fragment', 'slot_id'])
@@ -230,16 +228,13 @@ class Dataset:
             warnings.warn("runtime", RuntimeWarning)
             for k in ['raw_rms', 'int_rms', 'coh_rms', 'pedestal']:
                 median_e2e = group_e2e[k].transform('median').to_numpy()
-                medians = list()
-                for p in [0,1,2]:
-                    medians.append(np.mean(self.median_noise_data[self.median_noise_data['plane'] == p][k]))
-                median_c2c = np.array(medians)[self.noise_data['plane']]
+                median_c2c = group_c2c[k].transform('median').to_numpy()
                 self.noise_data[f'{k}_e2eabs'] = (self.noise_data[k].to_numpy() - median_e2e)
                 self.noise_data[f'{k}_c2cabs'] = (self.noise_data[k].to_numpy() - median_c2c)
                 self.noise_data[f'{k}_e2erel'] = (self.noise_data[k].to_numpy() - median_e2e) / median_e2e
                 self.noise_data[f'{k}_c2crel'] = (self.noise_data[k].to_numpy() - median_c2c) / median_c2c
 
-    def _get_correlation_matrix(self, crate=0) -> np.array:
+    def _get_correlation_matrix(self, crate=0, intrinsic=False) -> np.array:
         """
         Loads the correlation matrix containing channel-to-channel
         correlations for the desired crate.
@@ -248,6 +243,9 @@ class Dataset:
         ----------
         crate: int
             The crate number to load from the correlations dataset.
+        intrinsic: bool
+            Boolean for switching to the correlations after coherent
+            noise removal.
 
         Returns
         -------
@@ -256,7 +254,8 @@ class Dataset:
         """
         size = int(576 * 577 / 2)
         cov = np.zeros((576,576))
-        sel = (self.correlations['rho'] / self.correlations['count']).to_numpy()[crate*size:(crate+1)*size]
+        cor = self.correlations_int if intrinsic else self.correlations
+        sel = (cor['rho'] / cor['count']).to_numpy()[crate*size:(crate+1)*size]
         for si, s in enumerate(sel):
             i, j = self.indexer[si]
             if math.isnan(s):
@@ -288,7 +287,7 @@ class Dataset:
             offset += {'T': 0, 'M': 1, 'B': 2}[crate_name.upper()[4]]
         return offset
     
-    def plot_correlation_matrix(self, crate='WW19', save_path=None) -> None:
+    def plot_correlation_matrix(self, crate='WW19', save_path=None, spatial_permute=False, intrinsic=False) -> None:
         """
         Plot the channel-to-channel correlation matrix of either a
         single mini-crate or the average over all mini-crates.
@@ -300,6 +299,12 @@ class Dataset:
             if the average is desired.
         save_path: str
             The full path specifying the location to save the plot.
+        spatial_permute: bool
+            Boolean tag for turning on spatial permutation/ordering
+            of the channels within the crate.
+        intrinsic: bool
+            Boolean for switching to the correlations after coherent
+            noise removal.
     
         Returns
         -------
@@ -308,30 +313,56 @@ class Dataset:
         figure = plt.figure(figsize=(8,6))
         ax = figure.add_subplot()
 
+        conn = sqlite3.connect(SQLITE_CHANNEL_MAP_PATH)
+        chmap = pd.read_sql_query("SELECT channel_id, flange_name, slot_id*64+local_id FROM channelinfo;", conn).to_numpy()
+        conn.close()
+        chmap = {(x[1], x[2]) : x[0] for x in chmap}
+
         if crate.upper() == 'ALL':
-            title = 'All Crates'#'Average Mini-Crate Correlation Matrix'
-            cov = np.stack([self._get_correlation_matrix(i) for i in range(96)], axis=2)
+            title = 'All Crates'
+            cov = np.stack([self._get_correlation_matrix(i, intrinsic) for i in range(96)], axis=2)
         elif crate.upper() == 'IND1':
-            title = 'Induction 1'#'Average Induction 1 Mini-Crate Correlation Matrix'
+            title = 'Induction 1'
             crates = [x+y for x in ['EE', 'EW', 'WE', 'WW'] for y in ['01M', '01T', '20M', '20T']]
             crates = [self._map_crate(c) for c in crates]
-            cov = np.stack([self._get_correlation_matrix(c) for c in crates], axis=2)
+            cov = np.stack([self._get_correlation_matrix(c, intrinsic) for c in crates], axis=2)
         elif crate.upper() == 'CORNER':
-            title = 'Corner Crates'#'Average Corner Mini-Crate Correlation Matrix'
+            title = 'Corner Crates'
             crates = [x+y for x in ['EE', 'EW', 'WE', 'WW'] for y in ['01B', '01M', '01T', '20B', '20M', '20T']]
             crates = [self._map_crate(c) for c in crates]
-            cov = np.stack([self._get_correlation_matrix(c) for c in crates], axis=2)
+            cov = np.stack([self._get_correlation_matrix(c, intrinsic) for c in crates], axis=2)
         elif crate.upper() == 'STANDARD':
-            title = 'Standard Crates'#'Average Standard Mini-Crate Correlation Matrix'
+            title = 'Standard Crates'
             crates = [f'{x}{y:02}' for x in ['EE', 'EW', 'WE', 'WW'] for y in range(2,20)]
             crates = [self._map_crate(c) for c in crates]
-            cov = np.stack([self._get_correlation_matrix(c) for c in crates], axis=2)
+            cov = np.stack([self._get_correlation_matrix(c, intrinsic) for c in crates], axis=2)
+        elif crate.upper() == 'TOP':
+            title = 'Top Crates'
+            crates = [x+y for x in ['EE', 'EW', 'WE', 'WW'] for y in ['01T', '20T']]
+            crates = [self._map_crate(c) for c in crates]
+            cov = np.stack([self._get_correlation_matrix(c, intrinsic) for c in crates], axis=2)
+        elif crate.upper() == 'MIDDLE':
+            title = 'Middle Crates'
+            crates = [x+y for x in ['EE', 'EW', 'WE', 'WW'] for y in ['01M', '20M']]
+            crates = [self._map_crate(c) for c in crates]
+            cov = np.stack([self._get_correlation_matrix(c, intrinsic) for c in crates], axis=2)
+        elif crate.upper() == 'BOTTOM':
+            title = 'Bottom Crates'
+            crates = [x+y for x in ['EE', 'EW', 'WE', 'WW'] for y in ['01B', '20B']]
+            crates = [self._map_crate(c) for c in crates]
+            cov = np.stack([self._get_correlation_matrix(c, intrinsic) for c in crates], axis=2)
         else:
             title = f'{crate.upper()} Correlation Matrix'
-            crate = self._map_crate(crate)
-            cov = np.stack([self._get_correlation_matrix(crate),])
+            crates = [self._map_crate(crate),]
+            cov = np.stack([self._get_correlation_matrix(crates[0], intrinsic),], axis=2)
+            permute = np.argsort([chmap[(crate.upper(), i)] for i in range(cov.shape[0])])
 
-        cov = np.sum(cov, axis=2) / np.sum(cov != 0, axis=2)
+        if spatial_permute:
+            print(cov.shape)
+            cov = (np.sum(cov, axis=2) / np.sum(cov != 0, axis=2))[permute, permute]
+            print(cov.shape)
+        else:
+            cov = np.sum(cov, axis=2) / np.sum(cov != 0, axis=2)
 
         im = ax.imshow(cov, cmap='RdBu', vmin=-1.0, vmax=1.0)
         cbar = figure.colorbar(im, ax=ax)
